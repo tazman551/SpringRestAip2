5280a628040b0b9496c0db0c66d0c160a8f80162
tazman551 <tazman551@live.com>
2025-03-29 10:47:09 -0400

    service logic works to return all data needed to display table,loc,sche, and res

--- src/main/java/com/elderwood/restapi/repository/ReservationsRepository.java
+++ src/main/java/com/elderwood/restapi/repository/ReservationsRepository.java
@@ -2,12 +2,11 @@ package com.elderwood.restapi.repository;
 
 
 import java.util.Date;
-import java.util.List;
 import java.util.Set;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+//import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.elderwood.restapi.model.reservations;
